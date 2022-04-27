7622dde857bb38df3eb8d65a6a82463a3216b506
martindapol <martindapol@gmail.com>
2022-04-27 20:49:36 -0300

    Convertir Horas 24 Parte I

---
+++ hora24-12/index.js
@@ -0,0 +1,16 @@
+function procesar(){ //este es mi programa principal
+
+    //tomar las entradas:
+    let inputHora = document.getElementById('hora')
+    let inputMin = document.getElementById('minutos')
+
+    let valorHr = parseInt(inputHora.value) //obtiene un entero a partir de una cadena: "2"->2
+    let valorMin = parseInt(inputMin.value)
+
+    //Procesar las entradas:
+    alert('Valor hora: ' + valorHr)
+    alert('Valor min: ' + valorMin)
+    
+
+    //Mostrar los resultados:
+}
