55ab3857eefe462dd5e2027af77ef15ee72d2601
martindapol <martindapol@gmail.com>
2022-05-04 20:03:24 -0300

    Carga While parte I

---
+++ Carga while/index.js
@@ -0,0 +1,48 @@
+function procesar(){
+    let $cantidad = document.getElementById('cantidad')
+    let cantidad = parseInt($cantidad.value)
+
+    if(cantidad == NaN)
+    {
+        alert('Debe ingresar un valor numérico!')
+        return   
+    }
+
+    let contador = 0
+    let contPares = 0, contImpares = 0
+    let aviso0 = false //bandera para avisar que pasó el cero
+    let anterior = null
+    let paridad = null
+    let paridadAnterior = null
+    let avidoSecuenciaAlterados = true
+
+    while(contador < cantidad){ //se evalua de 0 a cantidad-1
+        //un valor  entero [0, 100]
+        let rand = Math.floor(Math.random() * 101) //Math.random() genera un valor de probabilidad [0;1) 
+        console.log(rand)
+        //validamos paridad del aleatorio generado:
+        
+        paridad = rand % 2 == 0
+        if(paridad == true) // equivale a if(paridad)
+            contPares++
+        else
+            contImpares++
+        
+        if(rand == 0)
+            aviso0 = true
+
+        paridadAnterior = anterior%2 == 0     
+        if(paridad == paridadAnterior)
+            avidoSecuenciaAlterados = false
+
+
+        anterior = rand
+        contador++ // contador = contador + 1
+    }//fin del while:
+
+    //RESULTADOS:
+    console.log('Cantidad de pares: ' + contPares)
+    console.log('Cantidad de impares: ' + contImpares)
+    console.log('Paso al menos una vez el 00 ?: ' + aviso0 ? 'SI': 'NO')
+    console.log('Hubo alternancia PAR/IMPAR ?: ' + aviso0 ? 'SI': 'NO')
+}
--- cifra_invertida/index.js
+++ cifra_invertida/index.js
@@ -4,11 +4,19 @@ function invertir(){
     let cifra = inputCifra.value
     let cifrainvertida = null
 
+    
     if(cifra < 100 || cifra > 999){
         alert("Por favor ingrese un valor valido.")
         return;
     }
-    cifrainvertida = cifra[2] + cifra[1] + cifra[0]
+    
+    let valorNumero =  parseInt(cifra)
+    let centena = parseInt(valorNumero/100)
+    let decena = parseInt(valorNumero%100/10)
+    let unidad = parseInt(valorNumero%100)%10
+    
+    //cifrainvertida = cifra[2] + cifra[1] + cifra[0]
+    cifrainvertida = unidad.toString() + decena.toString() + centena.toString()
     if(cifrainvertida == cifra){
         alert("El Numero original y el invertido son el mismo.")
     }
