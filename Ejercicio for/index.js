0df0e11fbd6418ec0d2b5a42e5512b680af297bb
martindapol <martindapol@gmail.com>
2022-05-27 19:04:47 -0300

    Ejercicio FOR

---
+++ Ejercicio for/index.js
@@ -0,0 +1,13 @@
+function procesar(){
+    let $cant = document.getElementById('cant') //caja cantidad
+    let n = parseInt($cant.value) // número de vueltas del ciclo for
+
+    for (let i = 0; i < n; i++) {
+       let temp = parseInt(Math.random()*50) // devuelve un número entre [0;1)
+       console.log(temp) 
+
+        //Desarrollar la lógica necesaria para resolver los 2 último puntos :
+
+    } 
+
+}
--- primer JS/index.js
+++ primer JS/index.js
@@ -1,31 +1,43 @@
 function convertir(){
     let $hora = document.getElementById('hora') //caja hora
     let $minutos = document.getElementById('min') //caja min
+    let $hora12 = document.getElementById('hora12') //caja min
+    
     
     let hora = parseInt($hora.value) //valor de la caja hora
     let min = parseInt($minutos.value) //valor de la caja min
 
     if(hora <0 || hora >23){
         alert('Valor de hora incorrecto!')
+        $hora.value = ''
         return
     }
     if(min <0 || min >59){
         alert('Valor de minutos incorrecto!')
+        $minutos.value = '' // asignar el texto de la caja en  blanco
         return
     }
 
     if(hora == 0){
-        //caso 1:
-        
+       hora = 12
+       amPm = 'AM'
     }else {
         if(hora <12){
             //caso2:
-
+            amPm = 'AM'
         }else{
             //caso 3:
-
+            if(hora == 12)
+                amPm = 'PM'
+            else{
+                //caso 4:
+                hora = hora - 12
+                amPm = 'PM'
+            }
         }
     }
-
-
+    let mensaje = `Hora[12]: ${hora}:${min} ${amPm}`   
+    //console.log(mensaje)
+    //alert(mensaje)
+    $hora12.value = mensaje
 }
